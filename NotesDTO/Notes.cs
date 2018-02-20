6296fad163895e6f32801ec6cdd19e9898e7069d
Ricky Melville <git@this-domain.net>
2018-02-20 06:58:33 +0000

    Seperated Actions from Notes

--- NotesDTO/Notes.cs
+++ NotesDTO/Notes.cs
@@ -25,6 +25,12 @@ namespace NotesCreatorDTO
         [Display(Name = "Note")]
         [Column("Note")]
         public string MainNote { get; set; }
+        public NoteActionDTO NoteAction { get; set; }
+    }
+
+    public class NoteActionDTO
+    {
+        public int ID { get; set; } // An entry of ID is automatically treated as the Key! //
 
         [DisplayFormat(DataFormatString = "{0:dd MMM yy hh:mm}", ApplyFormatInEditMode = true)]
         [Display(Name = "Action Date")]
@@ -39,5 +45,8 @@ namespace NotesCreatorDTO
 
         public Boolean Urgent { get; set; }
         public Boolean Callback { get; set; }
+
+        public int NoteID { get; set; }
+        public NoteDTO Note {get; set;}
     }
 }
